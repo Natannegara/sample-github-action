e0078823bb3b60ec99c2e4d75595e7ef292e020a
Natannegara_ <natannegara@gmail.com>
2022-08-29 13:41:34 +0800

    while looping get status

--- index.js
+++ index.js
@@ -2,6 +2,44 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const axios = require('axios');
 
+// const username = core.getInput('username');
+// const password = core.getInput('password');
+const username = 'admin';
+const password = 'admin';
+
+const url_launch = 'http://localhost/api/v2/job_templates/9/launch/'
+const headers = {
+  'Authorization': `Basic ZGV2ZWxvcGVyOk40dDRuM2c0cjQ=`,
+  'Content-Type': 'application/json'
+}
+
+axios({
+    method: 'post',
+    url: url_launch,
+    headers: headers
+}).then(async res => {
+        let id_job = res.data.id;
+        let resultStatus = ''
+        while (resultStatus != 'successful') {
+          // keep calling AWX to get status update
+          await axios({
+            method: 'get',
+            url: `http://localhost/api/v2/jobs/${id_job}`,
+            headers: headers
+          }).then(resJob => {
+            console.log('still looping, status:', resultStatus)
+            resultStatus = resJob.data.status
+            return resultStatus
+          }).catch(errJob=>{
+            console.log(errJob)
+          })
+        }
+        console.log('Job Template Status: ', resultStatus)
+
+    }).catch(err => {
+        console.log(err)
+    })
+
 // try {
 //     // `who-to-greet` input defined in action metadata file
 //     const nameToGreet = core.getInput('who-to-greet');
@@ -18,49 +56,5 @@ const axios = require('axios');
 // catch (error) {
 //     core.setFailed(error.message);
 // }
-
-try {
-
-    // const username = core.getInput('username');
-    // const password = core.getInput('password');
-    const username = core.getInput('admin');
-    const password = core.getInput('admin');
-    const buff = new Buffer(`${username}:${password}`, 'utf8');
-    const auth64 = buff.toString('base64');
-
-    axios({
-        method: 'post',
-        url: 'http://localhost/api/v2/job_templates/9/launch/',
-        headers:{
-            Authorization: `Basic ${auth64}`
-        }
-    })
-        .then(res => {
-            console.log(response.data);
-            // console.log(response.data.explanation)
-        })
-        .catch(err => {
-            console.log(err)
-        })
-
-} catch (error) {
-    core.setFailed(error.message);
-    
-}
-
-// axios({
-//     method: 'post',
-//     url: 'http://localhost/api/v2/job_templates/9/launch/',
-//     headers:{
-//         Authorization: "Basic ZGV2ZWxvcGVyOk40dDRuM2c0cjQ=" 
-//     }
-// })
-//     .then(response => {
-//         console.log(response.data.url);
-//         console.log(response.data.explanation)
-//     })
-//     .catch(err => {
-//         console.log(err)
-//     })
 
 
